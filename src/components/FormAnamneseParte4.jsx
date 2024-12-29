a72f6f9c76389dd8c448824198a02c64a96b589d
Abdeel Lucena porpino <105687102+AbdeelLucena@users.noreply.github.com>
2024-12-28 21:36:17 -0300

    Update FormAnamneseParte4.jsx

--- src/components/FormAnamneseParte4.jsx
+++ src/components/FormAnamneseParte4.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-// eslint-disable-next-line react/prop-types
+
 const FormAnamneseParte4 = ({ onNext, onPrev }) => {
   const [formData, setFormData] = useState({
     tratamentosAnteriores: "",
